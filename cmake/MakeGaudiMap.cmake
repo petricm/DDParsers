d5603ea30f6a5c9bda493e9fe6b3b12bb5002cda
Marko Petric <marko.petric@cern.ch>
2016-11-28 18:35:00 +0100

    Rename MakeRootMap to MakeGaudiMap for consistency

---
+++ cmake/MakeGaudiMap.cmake
@@ -0,0 +1,22 @@
+message(STATUS " *** Gaudi listcomponents: Generate map for ${libname} ..." )
+  #
+  #  Running listcomponents from Gaudi
+  #   
+  FIND_PROGRAM( Gaudi_listcomponents_CMD listcomponents PATHS ${genmap_install_dir}/../bin ${DD4hep_DIR}/bin )
+  ###MESSAGE( STATUS " *** MakeRootMap ${genmap_install_dir}/../bin $ENV{DD4hep_DIR}/bin ${DD4hep_DIR}" )
+  MESSAGE( STATUS " *** MakeGaudiMap.cmake run command : ${Gaudi_listcomponents_CMD} -o ${rootmapfile} ${libname}
+                 WORKING_DIRECTORY ${genmap_install_dir} "
+    ) 
+
+  if(APPLE)
+    SET ( ENV{DYLD_LIBRARY_PATH} ${genmap_install_dir}:$ENV{DYLD_LIBRARY_PATH}:$ENV{DD4HEP_LIBRARY_PATH} )
+  else()
+    SET ( ENV{LD_LIBRARY_PATH} ${genmap_install_dir}:$ENV{LD_LIBRARY_PATH} )
+  endif()
+  # EXECUTE_PROCESS( COMMAND echo LD_LIBRARY_PATH = $ENV{LD_LIBRARY_PATH} )
+  EXECUTE_PROCESS( COMMAND 
+    ${Gaudi_listcomponents_CMD} -o ${rootmapfile} ${libname}
+    WORKING_DIRECTORY ${genmap_install_dir}
+    )
+
+
